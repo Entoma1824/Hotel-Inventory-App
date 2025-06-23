d0939252bd39320d01da20eced6080bd78c598c0
Entoma <vienmarcustiongson@gmail.com>
2025-06-23 17:34:03 +0800

    Angular Demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  // template:`<h1>Hello world from inline Template!</h1>
+  // <p>Angular is awesome</p>
+  // `,
+
   styleUrl: './app.scss'
 })
 export class App {

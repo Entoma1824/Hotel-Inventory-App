d07dda18094be200b074e7b6df7cb37fa17aada8
Entoma <vienmarcustiongson@gmail.com>
2025-06-25 18:12:19 +0800

    Angular Demo binding syntax

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Rooms} from './rooms/rooms';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'hinv-root',
+  imports: [RouterOutlet, Rooms],
   templateUrl: './app.html',
   // template:`<h1>Hello world from inline Template!</h1>
   // <p>Angular is awesome</p>
   // `,
 
   styleUrl: './app.scss'
+  // styles: [`h1 { color: red }`]
 })
 export class App {
   protected title = 'hotelinventoryapp_alt';

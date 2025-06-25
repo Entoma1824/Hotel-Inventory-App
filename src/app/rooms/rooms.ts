235f4c0006dcec2fa76acdc8d6b201a1a3cdadb1
Entoma <vienmarcustiongson@gmail.com>
2025-06-25 18:13:42 +0800

    Angular Demo binding syntax

---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  imports: [],
+  templateUrl: './rooms.html',
+  styleUrl: './rooms.scss'
+})
+
+
+export class Rooms {
+  hotelName = 'Hilton hotel';
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}

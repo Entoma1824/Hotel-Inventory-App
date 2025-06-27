873efdcf994ebe7fa26e8f8473ea75cb7e919f3c
Entoma <vienmarcustiongson@gmail.com>
2025-06-27 21:47:01 +0800

    Angular Demo directives

--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Room, RoomList} from './rooms.interface';
 
 @Component({
   selector: 'hinv-rooms',
@@ -14,6 +15,43 @@ export class Rooms {
   numberOfRooms = 10;
 
   hideRooms = false;
+
+  rooms : Room = {
+    totalRooms: 20,
+    availableRooms : 10,
+    bookedRooms : 5
+
+  }
+
+  roomList :RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:'https://jp.pinterest.com/pin/728386939755379848/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:'https://jp.pinterest.com/pin/728386939755379848/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1500,
+      photos:'https://jp.pinterest.com/pin/728386939755379848/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+  ];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
